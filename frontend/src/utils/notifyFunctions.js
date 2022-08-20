a3f328491647fb3258a42953aa206cfba57505cf
saitonikko <saitonikkodev@gmail.com>
2022-08-20 19:25:41 +0800

    add mint function

--- frontend/src/utils/notifyFunctions.js
+++
@@ -1,8 +0,0 @@
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-export const notify = (status, comment) => {
-    if (status === 0) toast.success(comment);
-    if (status === 1) toast.warning(comment);
-    if (status === 2) toast.error(comment);
-}
